b156531dc93568b3f503ade198e8fdb6719c32ce
本馬遼太郎 <rajiku.blanc@gmail.com>
2022-01-07 23:22:45 +0900

    create form

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -6,19 +6,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
    private final IssueService issueService;
 
 //    Get /issues
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model){
         model.addAttribute("issueList",issueService.findAll());
         return "issues/list";
     }
+
+//    Get /issues/creationForm
+    @GetMapping("/creationForm")
+    public String showCreationForm (){
+      return "issues/creationForm";
+    };
+
+//    Post /issues
+    @PostMapping
+    public String create(IssueForm form, Model model){
+        // TODO データの永続化
+
+        return showList(model); // TODO リロードボタン対策が必要
+
+    };
 }
