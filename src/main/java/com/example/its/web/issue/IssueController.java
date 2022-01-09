e1e6c0b98ac537cc7ecb7d2882fdb2655866043a
本馬遼太郎 <rajiku.blanc@gmail.com>
2022-01-09 14:31:23 +0900

    フォームの修正

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,42 +1,41 @@
 package com.example.its.web.issue;
 
-import com.example.its.domain.issue.IssueEntity;
 import com.example.its.domain.issue.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
-   private final IssueService issueService;
+    private final IssueService issueService;
 
-//    Get /issues
+    //    Get /issues
     @GetMapping
-    public String showList(Model model){
-        model.addAttribute("issueList",issueService.findAll());
+    public String showList(Model model) {
+        model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
     }
 
-//    Get /issues/creationForm
+    //    Get /issues/creationForm
     @GetMapping("/creationForm")
-    public String showCreationForm (){
-      return "issues/creationForm";
-    };
+    public String showCreationForm(@ModelAttribute IssueForm form) {
+        return "issues/creationForm";
+    }
 
-//    Post /issues
+
+    //    Post /issues
     @PostMapping
-    public String create(IssueForm form, Model model){
+    public String create(IssueForm form, Model model) {
         // TODO データの永続化
 
         return showList(model); // TODO リロードボタン対策が必要
 
-    };
+    }
 }
